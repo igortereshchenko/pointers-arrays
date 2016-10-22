ca9d86a46daa4aa99aabb50cc639896b106dc560
igortereshchenko <tereshchenko.igor@gmail.com>
2016-10-22 20:01:38 +0300

    input and validation

--- arrays/array.cpp
+++ arrays/array.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
-	cout<<"My first application";
+	//defaults
+	int count_of_elements=0;
+
+	//inputs
+	cout<<"Enter count of elemetes"<<"\n";
+	cin>>count_of_elements;
+
+	//validation
+	if (count_of_elements<=0){
+		cout<<"Error in count of elements, bye";
+	}
+
+	//calculation
+
+
+	//output
+
+
 	getchar();
 	return 0;
 
